16e26891227f19fedadfdb9b74e213444ce397e5
Egor <yahor.herasimau@mail.by>
2014-02-27 02:15:19 +0300

    menu pause

--- ZombieDiary/Assets/scripts/Menu.cs
+++ ZombieDiary/Assets/scripts/Menu.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Menu : MonoBehaviour {
 	// Use this for initialization
 	public GameObject gamePanel;
+	public GameObject gameButtons;
 	void Start () {
 		gamePanel.SetActive(false);
 	}
@@ -13,12 +14,14 @@ public class Menu : MonoBehaviour {
 		int btnHeight = Screen.height/5;
 		int dist = Screen.height/6 - Screen.height/10;
 		bool btnStart = GUI.Button(new Rect(Screen.width/2 - btnWidth/2, dist, btnWidth, btnHeight), "Start");
-		GUI.Button(new Rect(Screen.width/2 - btnWidth/2, 3*dist + btnHeight, btnWidth, btnHeight), "Oprions");
-		GUI.Button(new Rect(Screen.width/2 - btnWidth/2, 5*dist + 2*btnHeight, btnWidth, btnHeight), "Exitы");
+		GUI.Button(new Rect(Screen.width/2 - btnWidth/2, 3*dist + btnHeight, btnWidth, btnHeight), "Options");
+		GUI.Button(new Rect(Screen.width/2 - btnWidth/2, 5*dist + 2*btnHeight, btnWidth, btnHeight), "Exit");
 		if(btnStart)
 		{
 			this.gameObject.SetActive(false);
+			gameButtons.SetActive(true);
 			gamePanel.SetActive(true);
+			Time.timeScale = 1;
 		}
 	}
 }
--- ZombieDiary/Assets/scripts/ToMainMenu.cs
+++ ZombieDiary/Assets/scripts/ToMainMenu.cs
@@ -12,11 +12,23 @@ public class ToMainMenu : MonoBehaviour {
 	void OnGUI () {
 		int btnWidth = Screen.width/4;
 		int btnHeight = Screen.height/10;
-		bool btnToMenu = GUI.Button(new Rect(Screen.width/20, Screen.height - btnHeight - btnHeight/3, btnWidth, btnHeight), "Menu");
+		bool btnPause = GUI.Button(new Rect(btnHeight/3, Screen.height - btnHeight - btnHeight/3, btnWidth, btnHeight), "Pause");
+		bool btnToMenu = GUI.Button(new Rect(Screen.width - btnWidth - btnHeight/3, Screen.height - btnHeight - btnHeight/3, btnWidth, btnHeight), "Menu");
 		if (btnToMenu)
 		{
+			GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
+			for (int i = 0; i < zombies.Length; i++)
+			{
+				Destroy(zombies[i].gameObject);
+			}
 			gamePanel.SetActive(false);
 			mainMenu.SetActive(true);
+		}
+		if(btnPause)
+		{
+			mainMenu.SetActive(true);
+			this.gameObject.SetActive(false);
+			Time.timeScale = 0;
 		}
 	}
 }
